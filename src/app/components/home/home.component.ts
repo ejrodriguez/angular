b74a703b57d41ba1a3896e9e719fd8de8590bf32
Edison <rodriguezjedison@hotmail.es>
2018-12-22 09:22:58 -0800

    s c

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+//import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styles: []
+})
+export class HomeComponent implements OnInit {
+/*
+  paises: any[] = [];
+  constructor( private http: HttpClient ) {
+    console.log('Constructor del home');
+    this.http.get('https://restcountries.eu/rest/v2/lang/es')
+    .subscribe( (resp: any) => {
+        this.paises = resp;
+        console.log(resp);
+    } )
+  }*/
+  constructor()
+  {
+
+  }
+
+  ngOnInit() {
+  }
+
+}
